da233c75bdfe1221db628ab3d9f4342b0d98b4d0
Gobbledyglomp <skoobidyboo@pm.me>
2025-09-27 13:30:21 +0200

    Add solution for Exercise 1.2

---
+++ part1/1.2/src/App.jsx
@@ -0,0 +1,58 @@
+// Header
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+// Courses
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1[0]} exercise={props.part1[1]}/>
+      <Part part={props.part2[0]} exercise={props.part2[1]}/>
+      <Part part={props.part3[0]} exercise={props.part3[1]}/>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.value}</p>
+    </>
+  )
+}
+
+// App
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <>
+      <Header course={course} />
+      <Content part1={[ part1, exercises1 ]} part2={[ part2, exercises2 ]} part3={[ part3, exercises3 ]} />
+      <Total value={exercises1 + exercises2 + exercises3} />
+    </>
+  )
+}
+
+export default App
