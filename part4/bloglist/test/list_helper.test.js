43cd83beb91599e5379320457b771c6c7adc712d
Gobbledyglomp <skoobidyboo@pm.me>
2025-10-19 20:08:31 +0200

    Add solution to Exercise 4.7

--- part4/bloglist/test/list_helper.test.js
+++ part4/bloglist/test/list_helper.test.js
@@ -417,3 +417,160 @@ describe('author with most blogs', () => {
             || (JSON.stringify(result) === JSON.stringify(expectedResult2)))
     })
 })
+
+describe('author with most likes', () => {
+    //
+    // Insert data
+    //
+    const emptyList = []
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        }
+    ]
+    const listWithManyBlogs = [
+        {
+            _id: '5a422a851b54a676234d17f7',
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: '5a422b891b54a676234d17fa',
+            title: 'First class tests',
+            author: 'Robert C. Martin',
+            url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: '5a422ba71b54a676234d17fb',
+            title: 'TDD harms architecture',
+            author: 'Robert C. Martin',
+            url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: '5a422bc61b54a676234d17fc',
+            title: 'Type wars',
+            author: 'Robert C. Martin',
+            url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+            likes: 2,
+            __v: 0
+        }
+    ]
+    const listWithManyBlogsAndTwoTopAuthors = [
+        {
+            _id: '5a422a851b54a676234d17f7',
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 17,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: '5a422b891b54a676234d17fa',
+            title: 'First class tests',
+            author: 'Robert C. Martin',
+            url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: '5a422ba71b54a676234d17fb',
+            title: 'TDD harms architecture',
+            author: 'Robert C. Martin',
+            url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: '5a422bc61b54a676234d17fc',
+            title: 'Type wars',
+            author: 'Robert C. Martin',
+            url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+            likes: 2,
+            __v: 0
+        }
+    ]
+
+    test('of the empty list is null', () => {
+        const result = listHelper.mostLikes(emptyList)
+        assert.strictEqual(result, null)
+    })
+
+    test('in the list with one blog is the author of the blog', () => {
+        const result = listHelper.mostLikes(listWithOneBlog)
+        const expectedResult = {
+            author: 'Edsger W. Dijkstra',
+            likes: 5
+        }
+
+        assert.deepStrictEqual(result, expectedResult)
+    })
+
+    test('in the list with many blogs is the correct author', () => {
+        const result = listHelper.mostLikes(listWithManyBlogs)
+        const expectedResult = {
+            author: 'Edsger W. Dijkstra',
+            likes: 17
+        }
+
+        assert.deepStrictEqual(result, expectedResult)
+    })
+
+    test('in the list with many blogs and 2 top authors is one of the correct authors', () => {
+        const result = listHelper.mostLikes(listWithManyBlogsAndTwoTopAuthors)
+        const expectedResult1 = {
+            author: 'Michael Chan',
+            likes: 17
+        }
+        const expectedResult2 = {
+            author: 'Edsger W. Dijkstra',
+            likes: 17
+        }
+
+        assert.ok((JSON.stringify(result) === JSON.stringify(expectedResult1))
+            || (JSON.stringify(result) === JSON.stringify(expectedResult2)))
+    })
+})
