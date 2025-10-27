0eaeca0355faf80637be9bdf4d75783eed7f0fa4
Gobbledyglomp <skoobidyboo@pm.me>
2025-10-27 22:17:18 +0100

    Add solution to Exercise 5.10

--- part5/bloglist/frontend/src/components/BlogList.jsx
+++ part5/bloglist/frontend/src/components/BlogList.jsx
@@ -82,12 +82,15 @@ const BlogList = ({ blogs, setBlogs, notify }) => {
 
     return (
         <div>
-            {blogs.map(blog => <Blog
-                key={blog.id}
-                blog={blog}
-                updateBlog={updateBlog}
-                notify={notify}
-            />)}
+            {blogs
+                .sort((a, b) => b.likes - a.likes)
+                .map(blog => <Blog
+                    key={blog.id}
+                    blog={blog}
+                    updateBlog={updateBlog}
+                    notify={notify}
+                />)
+            }
         </div>
     )
 }
