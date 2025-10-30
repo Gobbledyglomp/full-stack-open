4b5bd4bffe3b357ccfd4ab53483f5826e3d80d86
Gobbledyglomp <skoobidyboo@pm.me>
2025-10-30 21:05:34 +0100

    Add solution to Exercise 5.17

--- part5/bloglist-e2e-testing/tests/example.spec.js
+++ part5/bloglist-e2e-testing/tests/example.spec.js
@@ -1,19 +1,22 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
+const { test, expect, beforeEach, describe } = require('@playwright/test')
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  test('Login form is shown', async ({ page }) => {
+    await expect(page
+      .getByRole('heading', { name: 'Log in to application' }))
+      .toBeVisible()
+    await expect(page
+      .getByRole('textbox', { name: 'Username' }))
+      .toBeVisible()
+    await expect(page
+      .getByRole('textbox', { name: 'Password' }))
+      .toBeVisible()
+    await expect(page
+      .getByRole('button', { name: 'Login' }))
+      .toBeVisible()
+  })
+})
