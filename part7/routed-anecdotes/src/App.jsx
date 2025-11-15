6cae1224c7bb179c3d4b054ab6b4ad65b155efd8
Gobbledyglomp <skoobidyboo@pm.me>
2025-11-15 16:36:38 +0100

    Add solution to Exercise 7.4

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-
 import {
   Link,
   Routes,
@@ -7,6 +6,8 @@ import {
   useMatch,
   useNavigate,
 } from 'react-router-dom'
+
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -84,9 +85,9 @@ const Footer = () => (
 
 const CreateNew = ({ addNew }) => {
   // State
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
 
   // Navigate
   const navigate = useNavigate()
@@ -95,16 +96,13 @@ const CreateNew = ({ addNew }) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     })
 
     navigate('/')
-    setContent('')
-    setAuthor('')
-    setInfo('')
   }
 
   // Render
@@ -114,15 +112,15 @@ const CreateNew = ({ addNew }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
